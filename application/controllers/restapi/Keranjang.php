90183f8915001c1a5c3d0d6397c767a30df9ac7b
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2021-03-02 11:18:06 +0700

    keranjang

--- application/controllers/restapi/Keranjang.php
+++ application/controllers/restapi/Keranjang.php
@@ -13,11 +13,27 @@ class Keranjang extends RestController
 		$this->load->model('M_keranjang', 'keranjang');
 	}
 
-//	public function tampil_get()
-//	{
-//		$idUser = $this->get('idUser');
-//		$getData = $this->order->get_data_keranjang($idUser);
-//	}
+	public function tampil_get()
+	{
+		$idUser = $this->get('idUser');
+		$idKrj = $this->keranjang->get_data_tambahkeranjang($idUser)->row()->id_tkeranjang;
+		$getData = $this->keranjang->get_data_keranjang($idKrj);
+		if($getData->num_rows() != 0){
+			$dataResponse = [
+				'status' => 'success',
+				'message' => 'Data berhasil didapatkan !',
+				'result' => $getData->result_array()
+			];
+			$this->response($dataResponse, 200);
+		} else {
+			$dataResponse = [
+				'status' => 'failed',
+				'message' => 'Data kosong!',
+				'result' => []
+			];
+			$this->response($dataResponse, 404);
+		}
+	}
 
 	public function tambah_post()
 	{
@@ -37,10 +53,32 @@ class Keranjang extends RestController
 				//mengecek apakah id_produk sudah ada atau belum
 				$cekIdProduk = $this->keranjang->cek_id_produk($idProduk)->num_rows();
 				if($cekIdProduk != 0){
-					//id produk ada isinya
-					echo "id sudah ada coy";
+
+					##### saat keranjang idProduk ada maka akan diperbaharui jumlahnya#####
+					$jumlahProduk = $this->keranjang->cek_id_produk($idProduk)->row()->jumlah;
+					$NewJumlah = $jumlah + $jumlahProduk;
+					$NewHarga = $NewJumlah * $hargaProduk;
+					$dataKeranjangNew = [
+						'jumlah' => $NewJumlah,
+						'sub_total' => $NewHarga
+					];
+					$this->keranjang->update_keranjang($dataKeranjangNew, $idProduk);
+					$idKeranjang = $this->keranjang->get_data_tambahkeranjang($idUser)->row()->id_tkeranjang;
+					$subTotal = $this->keranjang->get_sub_total($idKeranjang);
+					$totalBiayaFix = $subTotal + $biayaAntar;
+					$dataTotalKeranjangNew = [
+						'sub_total' => $subTotal,
+						'biaya_antar' => $biayaAntar,
+						'total_biaya' => $totalBiayaFix
+					];
+					$this->keranjang->update_total_keranjang($dataTotalKeranjangNew,$idUser);
+					$dataResponse = [
+						'status' => 'success',
+						'message' => 'Data berhasil diupdate !',
+					];
+					$this->response($dataResponse, 200);
 				} else {
-					##### saat keranjang idProduk tidak ada #####
+					##### saat keranjang idProduk tidak ada maka akan ditambahkan ke dalam data keranjang#####
 					$idKeranjang = $this->keranjang->get_data_tambahkeranjang($idUser)->row()->id_tkeranjang;
 					$datakeranjang = [
 						'id_krj' => $idKeranjang,
@@ -61,7 +99,7 @@ class Keranjang extends RestController
 						$this->keranjang->update_total_keranjang($dataTKeranjang,$idUser);
 						$dataResponse = [
 							'status' => 'success',
-							'message' => 'Data gagal disimpan !',
+							'message' => 'Data berhasil diupdate !',
 						];
 						$this->response($dataResponse, 200);
 					} else{
@@ -80,7 +118,7 @@ class Keranjang extends RestController
 				'id_user' => $idUser,
 				'sub_total' => $hargaProduk,
 				'biaya_antar' => $biayaAntar,
-				'total_biaya' => $hargaTProduk
+				'total_biaya' => $hargaTProduk+$biayaAntar
 			];
 			$idTbKeranjang = $this->keranjang->simpan_total_keranjang($dataTotalKeranjang);
 			//menyimpan ke dalam keranjang
@@ -99,11 +137,103 @@ class Keranjang extends RestController
 				$this->response($dataResponse, 200);
 			} else{
 				$dataResponse = [
-					'status' => 'success',
+					'status' => 'failed',
 					'message' => 'Data gagal disimpan !',
 				];
 				$this->response($dataResponse, 404);
 			}
+		}
+	}
+
+	public function ubah_post()
+	{
+		$idUser = $this->post('idUser');
+		$idProduk = $this->post('idProduk');
+		$opsi = $this->post('opsi');
+
+		$dataKeranjang = $this->keranjang->get_keranjang_byidproduk($idProduk)->row();
+		//$biayaAntar = 20000;
+		$hargaProduk = $dataKeranjang->hargaProduk;
+		$jumlah = $dataKeranjang->jumlah;
+		$idKrj = $dataKeranjang->id_krj;
+		switch ($opsi) {
+			### Menambahkan jumlah produk ###
+			case 1:
+				$jumlah++;
+				$subTotalHarga = $jumlah * $hargaProduk;
+				//update keranjang
+				$dataKeranjang = [
+					'jumlah' => $jumlah,
+					'sub_total' => $subTotalHarga
+				];
+				$this->keranjang->update_keranjang($dataKeranjang, $idProduk);
+				//update data total_keranjang
+				$subTotal = $this->keranjang->get_sub_total($idKrj);
+				$dataTotalKeranjang = [
+					'sub_total' => $subTotal,
+					'total_biaya' => $subTotal+20000
+				];
+				$this->keranjang->update_total_keranjang($dataTotalKeranjang,$idUser);
+				$dataResponse1 = [
+					'status' => "success",
+					'message' => 'Produk ditambahkan !',
+				];
+				$this->response($dataResponse1, 200);
+				break;
+			case 2:
+				$jumlah--;
+				$subTotalHarga = $jumlah * $hargaProduk;
+				//update keranjang
+				$dataKeranjang = [
+					'jumlah' => $jumlah,
+					'sub_total' => $subTotalHarga
+				];
+				$this->keranjang->update_keranjang($dataKeranjang, $idProduk);
+				//update data total_keranjang
+				$subTotal = $this->keranjang->get_sub_total($idKrj);
+				$dataTotalKeranjang = [
+					'sub_total' => $subTotal,
+					'total_biaya' => $subTotal+20000
+				];
+				$this->keranjang->update_total_keranjang($dataTotalKeranjang,$idUser);
+				$dataResponse2 = [
+					'status' => "success",
+					'message' => 'Produk dikurangi !',
+				];
+				$this->response($dataResponse2, 200);
+				break;
+			case 3:
+				break;
+			default:
+				$dataResponse = [
+					'status' => 'failed',
+					'message' => 'Gagal Mendapatkan Data !',
+				];
+				$this->response($dataResponse, 404);
+				break;
+		}
+	}
+
+	public function total_get()
+	{
+		$idUser = $this->get('idUser');
+		$getData = $this->keranjang->get_data_totalkeranjang($idUser);
+		if($getData->num_rows() != 0){
+			$rowData = $getData->row();
+			$data['subTotalKeranjang'] = $rowData->sub_total;
+			$data['biayaAntarKeranjang'] = $rowData->biaya_antar;
+			$data['totalBiayaKeranjang'] = $rowData->total_biaya;
+			$data['status'] = "success";
+			$data['message'] = "Berhasil mendapatkan data!";
+
+			$this->response($data, 200);
+
+		}else {
+			$dataResponse = [
+				'status' => 'failed',
+				'message' => 'Gagal Mendapatkan Data !',
+			];
+			$this->response($dataResponse, 404);
 		}
 	}
 }
--- application/models/M_keranjang.php
+++ application/models/M_keranjang.php
@@ -9,11 +9,11 @@ class M_keranjang extends CI_Model
 		$this->load->database();
 	}
 
-	public function get_data_keranjang($idUser)
+	public function get_data_keranjang($idKeranjang)
 	{
-		$this->db->select("keranjang.id_produk as idProduk,judul,tanggal,total_jumlah as totalOrder, status as statusOrder");
+		$this->db->select("keranjang.id_produk as idProduk,produk.nama_produk as namaProduk,produk.total_harga as hargaProduk, produk.gambar as gambarProduk, keranjang.jumlah as jumlahProduk, keranjang.sub_total as subTotalProduk");
 		$this->db->join("produk", 'produk.id_produk=keranjang.id_produk');
-		$this->db->where('keranjang.id_user', $idUser);
+		$this->db->where('keranjang.id_krj', $idKeranjang);
 		return $this->db->get("keranjang");
 	}
 
@@ -44,16 +44,31 @@ class M_keranjang extends CI_Model
 		$this->db->select_sum('sub_total');
 		$result = $this->db->get('keranjang')->row();
 		return $result->sub_total;
-
-//		$this->db->select_sum("sub_total");
-//		$this->db->select("sub_total as TotalOrder");
-//		$this->db->where('id_krj', $idKrj);
-//		return $this->db->get("keranjang");
 	}
 
 	public function update_total_keranjang($dataTKeranjang,$idUser)
 	{
 		$this->db->where('id_user',$idUser);
 		$this->db->update("total_keranjang",$dataTKeranjang);
+	}
+
+	public function update_keranjang($dataKeranjangNew, $idProduk)
+	{
+		$this->db->where('id_produk',$idProduk);
+		$this->db->update("keranjang",$dataKeranjangNew);
+	}
+
+	public function get_keranjang_byidproduk($idProduk)
+	{
+		$this->db->select("keranjang.sub_total,keranjang.jumlah,keranjang.id_krj,produk.total_harga as hargaProduk");
+		$this->db->join("produk", 'produk.id_produk=keranjang.id_produk');
+		$this->db->where('keranjang.id_produk', $idProduk);
+		return $this->db->get("keranjang");
+	}
+
+	public function get_data_totalkeranjang($idUser)
+	{
+		$this->db->where('id_user',$idUser);
+		return $this->db->get("total_keranjang");
 	}
 }
